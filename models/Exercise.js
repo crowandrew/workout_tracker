3772294da4a4fc6b4e8f680efbf934604a1b069e
Andrew Crow <andrew@crow.me>
2020-10-29 13:47:00 -0700

    added Exercise model

---
+++ models/Exercise.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Activity Name"
+  },
+  count: {
+    type: Number,
+    required: "Activity Count"
+  },
+  unit: {
+    type: String,
+    required: "Units"
+  },
+  notes: String
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
