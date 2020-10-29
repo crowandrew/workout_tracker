9a86b3e4e8a7457e08a875500e6cf292c9930ad4
Andrew Crow <andrew@crow.me>
2020-10-29 13:45:24 -0700

    setup wokout model

---
+++ models/Workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    name: {
+        type: String,
+        required: "Please enter a name for this workout"
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ],
+    date: {
+      type: Date,
+      default: Date.now,
+      required: true
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
