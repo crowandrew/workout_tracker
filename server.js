9cb9ef4c2aab68b17a6f3df003cd18dc993476ea
Andrew Crow <andrew@crow.me>
2020-11-02 20:26:05 -0800

    deploying

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
